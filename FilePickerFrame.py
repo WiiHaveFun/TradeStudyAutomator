793d1c305fe0de8ea9a431c7e35f56406506b399
michaelchen <jr.michael.chen@gmail.com>
2023-05-09 14:07:21 -0400

    Add line numbers; fixed issue where cancelling file picker also clears out old file

--- FilePickerFrame.py
+++ FilePickerFrame.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog as fd
+import math
 
 
 class FilePickerFrame(ttk.Frame):
@@ -41,15 +42,15 @@ class FilePickerFrame(ttk.Frame):
         }
 
         # show the open file dialog
-        self.f = fd.askopenfile(filetypes=file_types)
+        f_name = fd.askopenfilename(filetypes=file_types)
 
-        # display file path and read file
-        if self.f is not None:
+        # display file path and read file if a file is picked
+        if f_name:
+            self.f = open(f_name)
             self.filePathLabel['text'] = "Path: " + self.f.name
             self.data = self.f.read()
             self.f.close()
 
-    # TODO Add line numbers
     def view_file(self):
         if self.f is not None:
             if self.readerWindow is None:
@@ -67,6 +68,24 @@ class FilePickerFrame(ttk.Frame):
         self.readerWindow.destroy()
         self.readerWindow = None
 
+    def view_text(self):
+        # Add line numbers
+        lines = self.data.split("\n")
+
+        order = math.floor(math.log(len(lines), 10))
+
+        for i in range(len(lines)):
+            lines[i] = str(i + 1).rjust(order + 1, " ") + "\t" + lines[i]
+        numbered_data = "\n".join(lines)
+
+        self.readerWindow.columnconfigure(0, weight=1)
+        self.readerWindow.rowconfigure(0, weight=1)
+
+        text = tk.Text(self.readerWindow)
+        text.grid(column=0, row=0, sticky=tk.NSEW)
+        text.insert(tk.END, numbered_data)
+        text.configure(state=tk.DISABLED)
+
     # TODO Make scrollable
     def view_csv(self):
         data_array = self.get_csv_data()
@@ -76,16 +95,6 @@ class FilePickerFrame(ttk.Frame):
                 ttk.Label(self.readerWindow, text=r).grid(column=0, row=r, sticky=tk.W, padx=5)
             for c, entry in enumerate(row_array):
                 ttk.Label(self.readerWindow, text=entry).grid(column=c + 1, row=r, padx=20)
-                # self.readerWindow.columnconfigure(c, weight=1)
-
-    def view_text(self):
-        self.readerWindow.columnconfigure(0, weight=1)
-        self.readerWindow.rowconfigure(0, weight=1)
-
-        text = tk.Text(self.readerWindow)
-        text.grid(column=0, row=0, sticky=tk.NSEW)
-        text.insert(tk.END, self.data)
-        text.configure(state=tk.DISABLED)
 
     def get_data(self):
         return self.data
