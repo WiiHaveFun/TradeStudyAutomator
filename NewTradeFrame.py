fb65432392f77c888480fbfb99ce06ba0a2ea37a
michaelchen <jr.michael.chen@gmail.com>
2024-05-23 00:56:20 -0500

    Add mass file picker

--- NewTradeFrame.py
+++ NewTradeFrame.py
@@ -15,6 +15,9 @@ class NewTradeFrame(ttk.Frame):
         self.avl_header = ttk.Label(self, text="Select avl file")
         self.avl_picker_frame = FilePickerFrame(self, "AVL files", "*.avl")
 
+        self.mass_header = ttk.Label(self, text="Select mass file (optional)")
+        self.mass_picker_frame = FilePickerFrame(self, "Mass files", "*.mass")
+
         self.line_edit_header = ttk.Label(self, text="Line edits")
         self.line_edits_frame = LineEditsFrame(self, self.csv_picker_frame, self.avl_picker_frame)
 
@@ -29,8 +32,11 @@ class NewTradeFrame(ttk.Frame):
         self.avl_header.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
         self.avl_picker_frame.grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
 
-        self.line_edit_header.grid(column=0, row=4, sticky=tk.W, padx=5, pady=5)
-        self.line_edits_frame.grid(column=0, row=5, sticky=tk.W, padx=5, pady=5)
+        self.mass_header.grid(column=0, row=4, sticky=tk.W, padx=5, pady=5)
+        self.mass_picker_frame.grid(column=0, row=5, sticky=tk.W, padx=5, pady=5)
+
+        self.line_edit_header.grid(column=0, row=6, sticky=tk.W, padx=5, pady=5)
+        self.line_edits_frame.grid(column=0, row=7, sticky=tk.W, padx=5, pady=5)
 
         # Place frame in window
         self.grid(column=0, row=0, sticky=tk.W)
